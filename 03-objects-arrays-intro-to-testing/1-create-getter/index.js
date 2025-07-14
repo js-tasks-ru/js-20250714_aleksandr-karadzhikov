e30403cea0d45f38e1092e76b38a8ac449fb89cb
Aleksandr Karadzhikov <aleksandr.karadzhikov@homebuddy.com>
2025-07-14 18:15:53 +0500

    [03-objects-array-intro-testing/1-create-getter]: implementation of createGetter util

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,29 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
 
+const isEmpty = (obj) => Object.keys(obj).length === 0;
+
+export function createGetter(path) {
+  return function (obj) {
+    if (isEmpty(obj)) {
+      return undefined;
+    }
+
+    const keys = path.split(".");
+    let current = obj;
+    let currentKey = keys.shift();
+
+    while (
+      current &&
+      typeof current === "object" &&
+      !Array.isArray(current) &&
+      currentKey
+    ) {
+      current = current[currentKey];
+      currentKey = keys.shift();
+    }
+
+    return current;
+  };
 }
