a2ecec054ac73151e6d9271156d33675e3f6192a
Aleksandr Karadzhikov <aleksandr.karadzhikov@homebuddy.com>
2025-07-14 11:13:55 +0500

    [02-js-data-types/3-omit]: implementation of omit util

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -4,6 +4,7 @@
  * @param {...string} fields - the properties paths to omit
  * @returns {object} - returns the new object
  */
-export const omit = (obj, ...fields) => {
-
-};
+export const omit = (obj, ...fields) =>
+  Object.fromEntries(
+    Object.entries(obj).filter(([key, _]) => !fields.includes(key))
+  );
