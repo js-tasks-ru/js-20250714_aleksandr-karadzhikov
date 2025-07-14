5a5542abd09ee1f11689b44cdc67791b9294757f
Aleksandr Karadzhikov <aleksandr.karadzhikov@homebuddy.com>
2025-07-14 11:00:56 +0500

    [02-js-data-types/1-sort-strings]: implementation of sortStrings util

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,16 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+export function sortStrings(arr, param = "asc") {
+  const collator = new Intl.Collator(["ru", "en"], {
+    sensitivity: "variant",
+    caseFirst: "upper",
+  });
+
+  const comparingFunc = (a, b) => collator.compare(a, b);
+
+  const sorted = [...arr].sort(comparingFunc);
+
+  return param === "desc" ? sorted.reverse() : sorted;
 }
