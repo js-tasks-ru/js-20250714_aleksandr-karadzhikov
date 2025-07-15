3375a113772907ee4a609cbd02508b39610e43fd
Aleksandr Karadzhikov <aleksandr.karadzhikov@homebuddy.com>
2025-07-15 18:33:12 +0500

    [03-objects-arrays-intro-to-testing]: implementation of uniq util

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,24 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (typeof arr === "undefined" || arr.length === 0) {
+    return [];
+  }
 
+  const result = [];
+  let currentResIndex = 0;
+
+  for (let index = 0; index < arr.length; index++) {
+    const value = arr[index];
+
+    if (index === 0) {
+      result.push(value);
+      currentResIndex += 1;
+    } else if (value !== result[currentResIndex - 1]) {
+      result.push(value);
+      currentResIndex += 1;
+    }
+  }
+
+  return result;
 }
