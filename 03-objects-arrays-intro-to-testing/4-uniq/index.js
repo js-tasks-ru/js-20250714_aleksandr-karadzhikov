0796d4d0eefd6272519e4588791bf5f97b4bd5b3
Aleksandr Karadzhikov <aleksandr.karadzhikov@homebuddy.com>
2025-07-19 11:56:19 +0500

    [03-objects-arrays-intro-to-testing]: rewrite uniq function using Set

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,24 +4,5 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-  if (typeof arr === "undefined" || arr.length === 0) {
-    return [];
-  }
-
-  const result = [];
-  let currentResIndex = 0;
-
-  for (let index = 0; index < arr.length; index++) {
-    const value = arr[index];
-
-    if (index === 0) {
-      result.push(value);
-      currentResIndex += 1;
-    } else if (value !== result[currentResIndex - 1]) {
-      result.push(value);
-      currentResIndex += 1;
-    }
-  }
-
-  return result;
+  return Array.from(new Set(arr));
 }
